c510d41eca84fbdd7c42216d3662664db271a005
alejandrodgz <5199@holbertonstudents.com>
2023-03-29 14:15:38 -0500

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+The main function of the script. It checks if the input Markdown file exists.
+"""
+
 from sys import argv, stderr, exit
 from os import path
 
 
 def main():
+    # Check if the correct number of arguments have been passed
     if len(argv) < 2:
         stderr.write('Usage: ./markdown2html.py README.md README.html')
         exit(1)
