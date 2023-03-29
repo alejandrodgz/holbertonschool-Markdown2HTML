d302af624794af3664def697d686f0e18c4d75f1
alejandrodgz <5199@holbertonstudents.com>
2023-03-29 14:09:55 -0500

    task 0

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv, stderr, exit
+from os import path
+
+
+def main():
+    if len(argv) < 2:
+        stderr.write('Usage: ./markdown2html.py README.md README.html')
+        exit(1)
+
+    if not (path.exists(argv[1])):
+        print(f'Missing {argv[1]}')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
