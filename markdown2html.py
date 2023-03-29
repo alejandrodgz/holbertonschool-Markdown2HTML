ee5312b89538b37219f68b4ebd1606c2d1c44eac
alejandrodgz <5199@holbertonstudents.com>
2023-03-29 18:45:40 -0500

    task2

--- markdown2html.py
+++ markdown2html.py
@@ -18,12 +18,24 @@ def main():
         exit(1)
 
     with open(argv[1], 'r') as f:
-        for line in f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
             if line[0] == '#':
                 hash_number = line.count('#')
                 heading_text = line[hash_number+1:].strip()
                 with open(argv[2], 'a') as out:
-                    out.writelines(f'<h{hash_number}>{heading_text}</h{hash_number}>\n')
+                    out.write(f'<h{hash_number}>{heading_text}</h{hash_number}>\n')
+            elif line[0] == '-':
+                    text_list = line[2:].strip()
+                    if lines[i - 1][0] != '-':
+                        with open(argv[2], 'a') as out:
+                            out.write(f'<ul>\n')
+                    with open(argv[2], 'a')as out:
+                            out.write(f'<li>{text_list}</li>\n')
+                    if i >= len(lines) - 1 or lines[i + 1][0] != '-':
+                        with open(argv[2], 'a') as out:
+                            out.write(f'</ul>\n')
+                    
 
 
 if __name__ == '__main__':
